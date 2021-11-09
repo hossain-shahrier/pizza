428955d723b3a2bf665e74539f3ce6f61f3511ba
hossain-shahrier <hossainshahrier.shm@gmail.com>
2021-11-09 20:37:54 +0600

    ENV File created

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // create a new express server
 const express = require("express");
 const app = express();
+// port
+const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
 // listen on port 3000
-app.listen(3000, () => {});
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
