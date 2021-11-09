6f5cad2dcb60b3358a1a2b5220c779d1f2ab677a
hossain-shahrier <hossainshahrier.shm@gmail.com>
2021-11-09 20:35:32 +0600

    Server.js File start

---
+++ server.js
@@ -0,0 +1,9 @@
+// create a new express server
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+// listen on port 3000
+app.listen(3000, () => {});
